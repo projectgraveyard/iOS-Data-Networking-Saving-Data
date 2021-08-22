257a8234c18a16c175aef05ee7f0b2d727235411
Dominic Swaine <dominic.swaine@gmail.com>
2021-08-22 17:06:05 +0100

    Complete lesson 5 (Data and Data Types)

--- Lesson-05/data-and-data-types.playground/Contents.swift
+++ Lesson-05/data-and-data-types.playground/Contents.swift
@@ -1,31 +1,3 @@
-/// Copyright (c) 2019 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import Foundation
 //: ## Sample
 let remindersDataURL = URL(fileURLWithPath: "Reminders", relativeTo: FileManager.documentsDirectoryURL)
@@ -44,11 +16,28 @@ challengeURL.lastPathComponent
 //: ## Data Types
 //: ### Integers
 let age: UInt8 = 32
+MemoryLayout.size(ofValue: age)
+MemoryLayout<UInt8>.size
+UInt8.min
+UInt8.max
+
 let height: Int8 = 72
+MemoryLayout.size(ofValue: height)
+MemoryLayout<Int8>.size
+UInt8.min
+UInt8.max
 //: ### Floats
 let weight: Float = 154.5
+MemoryLayout.size(ofValue: weight)
+MemoryLayout<Float>.size
+Float.leastNormalMagnitude
+Float.greatestFiniteMagnitude
 //: ### Doubles
 let earthRadius: Double = 3958.8
+MemoryLayout.size(ofValue: earthRadius)
+MemoryLayout<Double>.size
+Double.leastNormalMagnitude
+Double.greatestFiniteMagnitude
 //: ### Binary & Hexadecimal
 let ageBinary: UInt8 = 0b0010_0000
 let ageBinaryNegative: Int8 = -0b0010_0000
@@ -61,3 +50,5 @@ let favoriteBytes: [UInt8] = [
   0b1111_0000,  0b1001_1111,  0b1001_1000,  186,
   0xF0,         0x9F,         0x98,         187
 ]
+
+MemoryLayout<UInt8>.size * favoriteBytes.count
