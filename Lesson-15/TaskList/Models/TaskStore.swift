eb1abc77e3651e236b84c07fdc339a2da48a11a5
Dominic Swaine <dominic.swaine@gmail.com>
2021-08-30 16:51:19 +0100

    Complete lesson 15 (Challenge: Encoding JSON Arrays)

--- Lesson-15/TaskList/Models/TaskStore.swift
+++ Lesson-15/TaskList/Models/TaskStore.swift
@@ -1,31 +1,3 @@
-/// Copyright (c) 2019 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import Combine
 import Foundation
 
@@ -35,12 +7,13 @@ class TaskStore: ObservableObject {
   
   @Published var prioritizedTasks: [PrioritizedTasks] = [] {
     didSet {
-      saveJSONPrioritizedTask()
+      saveJSONPrioritizedTasks()
     }
   }
   
   init() {
     loadJSONPrioritizedTasks()
+    //print(FileManager.documentsDirectoryURL)
   }
   
   func getIndex(for priority: Task.Priority) -> Int {
@@ -58,15 +31,13 @@ class TaskStore: ObservableObject {
     }
   }
   
-  private func saveJSONPrioritizedTask() {
+  private func saveJSONPrioritizedTasks() {
     let encoder = JSONEncoder()
+    encoder.outputFormatting = .prettyPrinted
 
     do {
-      let taskData = try encoder.encode(prioritizedTasks.first?.tasks.last)
-      let taskJSONURL = URL(fileURLWithPath: "Task",
-                            relativeTo: FileManager.documentsDirectoryURL).appendingPathExtension("json")
-      
-      try taskData.write(to: taskJSONURL, options: .atomicWrite)
+      let tasksData = try encoder.encode(prioritizedTasks)
+      try tasksData.write(to: tasksJSONURL, options: .atomicWrite)
     } catch let error {
       print(error)
     }
